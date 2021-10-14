ecc2ba01c8d1d77a0f64cd5ed22aad8f5b315488
codybum <cbumgardner@gmail.com>
2021-10-14 10:41:55 -0400

    test

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -10,7 +10,7 @@ public class main {
     public static void main(String[] args) {
         // write your code here
 
-        //getGpuInfo();
+        getGpuInfo();
 
         getGpuMemoryUsage();
 
